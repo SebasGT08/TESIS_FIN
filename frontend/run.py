de820e6260b36310017d58a0a2c9254fc6a98ae6
SebasGT08 <guaman.sebastian802@gmail.com>
2025-01-26 18:45:32 -0500

    Permitir conexion local

--- frontend/run.py
+++ frontend/run.py
@@ -3,4 +3,4 @@ from app import app
 import callbacks  # Importa los callbacks para que se registren en la app
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(host="0.0.0.0", port=8050,debug=True)
